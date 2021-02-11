ede0580e00f6ab3c10a48fcd3b37371f12cde6de
diana11 <dynasatapathy186@gmail.com>
2021-02-11 15:56:32 +0530

    Added a functional clock

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+
+    var date = new Date();
+    // console.log(date);
+
+    let hr = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+
+    let hrPosition = hr*360/12 + (min*(360/60)/12);
+    let minPosition = min*360/60 + (sec*(360/60)/60);
+    let secPosition = sec*360/60;
+
+function RunningTheClock() {
+
+    // console.log("Hour: "+ hr + " minute: " +min+" second: "+sec);
+    hrPosition = hrPosition + (3/360);
+    minPosition = minPosition+(6/60);
+    secPosition = secPosition+6;
+
+    HOURHAND.style.transform = "rotate("+ hrPosition +"deg)";
+    MINUTEHAND.style.transform = "rotate("+ minPosition +"deg)";
+    SECONDHAND.style.transform = "rotate("+ secPosition +"deg)";
+}
+
+var iter = setInterval(RunningTheClock, 1000);
